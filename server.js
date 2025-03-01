4f193187ec4449be74a4b7089a5a8f09ec8afc09
Young Wolf <162513105+kingofdead6@users.noreply.github.com>
2025-03-01 14:53:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use(
+    cors({
+      origin: "https://kingofdead6.github.io/SoftWeb_Front/", 
+      methods: "POST",
+      allowedHeaders: "Content-Type",
+    })
+  );app.use(bodyParser.json());
 
 // Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
